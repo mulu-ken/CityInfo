124c7152d4790e6a6a305c37c7658b83700b0d3f
mulu-ken <mamom@oregonstate.edu>
2023-07-03 11:37:01 -0700

    Getting acquainted with EF Core

--- CityInfo.API/Entities/PointOfInterest.cs
+++ CityInfo.API/Entities/PointOfInterest.cs
@@ -13,10 +13,14 @@ namespace CityInfo.API.Entities
         [MaxLength(50)]
         public string Name { get; set; }
 
+        [MaxLength(200)]
+        public string Description { get; set; }
+
         [ForeignKey("CityId")]
         public City? City { get; set; }
         public int CityId { get; set; }
 
+        
         public PointOfInterest(string name)
         {
             Name = name;
